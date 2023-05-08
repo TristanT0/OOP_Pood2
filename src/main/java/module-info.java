118a4e543252b7c88a90bf20397a9e2d5adae31c
TristanT0 <tristan.toruke@ut.ee>
2023-05-08 19:44:56 +0300

    yes
    yes

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pood.eepilinepood2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens pood.eepilinepood2 to javafx.fxml;
-    exports pood.eepilinepood2;
-}
